5157b7ff3c657d0cdc8b2fdedd777b5355cb6e25
pierre_caby <pierrecaby@hotmail.com>
2020-11-30 14:02:11 +0100

    creation de la classe PierreCalc

---
+++ src/com/cda/calc/PierreCalculateur.java
@@ -0,0 +1,12 @@
+package com.cda.calc;
+
+public class PierreCalculateur implements ICalculateur{
+
+	@Override
+	public String calc(String param) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
